38da8e0e8fde95fa7f70f98d023f9f9d728bade6
Leandro <Leandro@leandro.com>
2024-05-14 14:45:05 -0300

    Create get e set

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,6 @@
-import { Entity } from "../../core/entities/entity"
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
-import { Optional } from "../../core/types/optional"
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Optional } from "@/core/types/optional"
 
 interface AnswerProps {
   authorId: UniqueEntityID
@@ -13,6 +13,38 @@ interface AnswerProps {
 export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get excerpt() {
+    return this.content
+      .substring(0, 120)
+      .trim()
+      .concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
   }
 
   static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
