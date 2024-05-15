50946516c2ca043317618772068ea3bc4fc6ed79
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2024-05-15 20:07:06 -0300

    Gerando dados ficticios

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,15 +1,19 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Question, QuestionProps } from '@/domain/forum/enterprise/entities/question';
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
+import { faker } from '@faker-js/faker';
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID  
+) {
   const question = Question.create({
     authorId: new UniqueEntityID(),
-    title: 'Example question',
+    title: faker.lorem.sentence(5),
     slug: Slug.create('example-question'),
-    content: 'Example content',
+    content: faker.lorem.text(),
     ...override
-  });
+  }, id);
 
   return question;
 }
